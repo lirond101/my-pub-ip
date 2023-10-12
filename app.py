1cbbf9a597a165c9ff88ba958cf9adfd69f5f733
lirond101 <lirond101@gmail.com>
2023-10-12 23:34:43 +0300

    nginx as container

--- app.py
+++ app.py
@@ -7,12 +7,18 @@ import ifcfg
 app = Flask(__name__)
 
 logger = init_logging()
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/', methods=['GET'])
 def get_pub_ip():
     try:
+        app.logger.debug(request.headers)
+        logger.debug(request.headers)
+        print(request.headers)
         if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
             return jsonify({'my public ip': request.environ['REMOTE_ADDR']}), 200
+        elif ',' in request.environ.get('HTTP_X_FORWARDED_FOR'):
+            return jsonify({'my public ip': request.environ['HTTP_X_FORWARDED_FOR'][:request.environ['HTTP_X_FORWARDED_FOR'].index(',')-1]}), 200
         else:
             return jsonify({'my public ip': request.environ['HTTP_X_FORWARDED_FOR']}), 200
     except Exception as ex:
